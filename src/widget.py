c6451571b5a8c497f3d43235ec5afe9fbef5ceca
Kirill <kirill.rylskij96@gmail.com>
2025-11-04 18:37:01 +0300

    add funcions in widget.py

--- src/widget.py
+++ src/widget.py
@@ -1,5 +1,11 @@
+from src.masks import get_mask_account, get_mask_card_number
+
+
 def mask_account_card(card_number: str) -> str:
     """Функция возвращает маску. Для карты XXXX XX** **** XXXX. Для счета **XXXX"""
     number = card_number[-1]
     name = card_number[:-1]
+    if  name.lower().startswith(("счет", "счёт")):
+        mask_number = f
+
 
