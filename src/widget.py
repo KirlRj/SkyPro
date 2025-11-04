491aa610609845d8b25a69305799cf4c24815bf6
Kirill <kirill.rylskij96@gmail.com>
2025-11-04 18:48:13 +0300

    modified function mask_account_card

--- src/widget.py
+++ src/widget.py
@@ -3,14 +3,16 @@ from src.masks import get_mask_account, get_mask_card_number
 
 def mask_account_card(card_number: str) -> str:
     """Функция возвращает маску. Для карты XXXX XX** **** XXXX. Для счета **XXXX"""
-    number = card_number[-1]
-    name = card_number[:-1]
+    info = card_number.split()
+    number = info[-1]
+    name = " ".join(info[:-1])
+
     if  name.lower().startswith(("счет", "счёт")):
         mask_number = get_mask_account(number)
     else:
         mask_number = get_mask_card_number(number)
     return f"{name} {mask_number}"
 
-
+print (mask_account_card(("Maestro 1596837868705199")))
 
 
