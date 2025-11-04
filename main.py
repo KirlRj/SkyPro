086812afade2df6c1a5cfde1b4bb2bfdb113feae
Kirill <kirill.rylskij96@gmail.com>
2025-11-04 20:24:54 +0300

    add new functions in main

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from src.masks import get_mask_account, get_mask_card_number
+from src.widget import get_date, mask_account_card
 
 print(get_mask_card_number("1234567890111342"))
-print(get_mask_account("1234567890111342"))
+print(get_mask_account("12345678901113421234"))
+print(mask_account_card("Visa Classic 6831982476737658"))
+print(mask_account_card("Счет 73654108430135871342"))
+print(get_date("2024-03-11T02:26:18.671407"))
