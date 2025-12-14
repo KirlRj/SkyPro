b18a97f65dc78647b678a5002a264688a080109e
Kirill <kirill.rylskij96@gmail.com>
2025-12-14 18:25:03 +0300

    add eternal_api.py and .env.example

---
+++ src/external_api.py
@@ -0,0 +1,30 @@
+import os
+
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+api_key = os.getenv("API_KEY")
+api_url = os.getenv("API_URL")
+
+
+def transfer_currency(transaction: dict) -> float:
+    if not api_url:
+        raise ValueError("API_URL не определен!")
+    if not api_key:
+        raise ValueError("API_KEY не определен")
+
+    operation = transaction["operationAmount"]
+    amount = float(operation["amount"])
+    currency = operation["currency"]
+
+    if currency["code"] != "RUB":
+        params = {"from": currency["code"], "to": "RUB", "amount": amount}
+        headers = {
+            "apikey": api_key,
+        }
+        response = requests.get(api_url, params=params, headers=headers)
+        return round(float(response.json()["result"]), 2)
+    else:
+        return float(amount)
